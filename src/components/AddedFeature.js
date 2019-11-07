36345b412147eb9fb3fcd520a9b34197ba78c0b6
ngaivoronski <ngaivoronski@gmail.com>
2019-11-06 21:51:49 -0500

    mvp

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { connect, useDispatch } from 'react-redux';
+import { removeFeature } from '../actions/actions'
 
 const AddedFeature = props => {
+
+  const Dispatch = useDispatch();
+  
+  const removeFeature = item => {
+    Dispatch({type:"REMOVE_ITEM",payload:item})
+  };
+
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={() => removeFeature(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { connect, useDispatch } from 'react-redux';
+import { buyItem } from '../actions/actions'
 
 const AdditionalFeature = props => {
+
+  const Dispatch = useDispatch();
+  
+  const buyItem = item => {
+    Dispatch({type:"ADD_ITEM",payload:item})
+  };
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={() => buyItem(props.feature)}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+// const mapDispatchToProps = {
+//   buyItem: Dispatch({type:"ADD_ITEM",payload:item})
+// };
+
+export default connect(
+  state => state,
+  // mapDispatchToProps
+)(AdditionalFeature);
